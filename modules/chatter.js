d205bd8a259fc1aaaa8b23275153cc9b29c64da6
ryanc16 <rconklin1994@yahoo.com>
2017-02-24 15:25:51 -0500

    Version 1.2.3 fixes made to Chatter module for emoji elements when used with a chat in an iframe

--- modules/chatter.js
+++ modules/chatter.js
@@ -1,5 +1,5 @@
 var Chatter = function(){
-	this.VERSION = "1.1.0";
+	this.VERSION = "1.1.2";
 	this.loaded = false;
 	this.settings = chrome.storage.local;
 	this.enabled;
@@ -7,7 +7,7 @@ var Chatter = function(){
 
 	this.updateloop;
 	this.user_count = 0;
-	this.colors = [ "YellowGreen","DarkGoldenRod","Chocolate","LimeGreen","CornflowerBlue","Violet","LightSkyBlue","SaddleBrown","DarkOrange","SeaGreen","MediumVioletRed","DarkTurquoise","Olive","Tomato","DarkOliveGreen","Turquoise","DarkRed","Purple","Maroon","RebeccaPurple","DarkCyan","MediumBlue","HotPink","DeepPink","CadetBlue","PaleVioletRed","DarkOrchid","MediumPurple","Brown","DeepSkyBlue","DodgerBlue","SlateBlue","OliveDrab","OrangeRed","Coral","Crimson","Red","LightCoral","FireBrick","LightSeaGreen","MediumSlateBlue","DarkMagenta","Teal","Orchid","MidnightBlue","Orange","MediumAquaMarine","Green","DarkSalmon","DarkBlue","Navy","SteelBlue","Fuchsia","SkyBlue","ForestGreen","MediumSeaGreen","DarkViolet","RosyBrown","DarkSlateBlue","DarkSeaGreen","RoyalBlue","DarkGreen","MediumOrchid","BlueViolet"];
+	this.colors = [ "YellowGreen","SteelBlue","Crimson","LimeGreen","CornflowerBlue","Violet","LightSkyBlue","SaddleBrown","DarkOrange","SeaGreen","MediumVioletRed","DarkTurquoise","Olive","Tomato","DarkOliveGreen","Turquoise","DarkRed","Purple","Maroon","RebeccaPurple","DarkCyan","MediumBlue","HotPink","DeepPink","CadetBlue","PaleVioletRed","DarkOrchid","MediumPurple","Brown","DeepSkyBlue","DodgerBlue","SlateBlue","OliveDrab","OrangeRed","Coral","Chocolate","Red","LightCoral","FireBrick","LightSeaGreen","MediumSlateBlue","DarkMagenta","Teal","Orchid","MidnightBlue","Orange","MediumAquaMarine","Green","DarkSalmon","DarkBlue","Navy","DarkGoldenRod","Fuchsia","SkyBlue","ForestGreen","MediumSeaGreen","DarkViolet","RosyBrown","DarkSlateBlue","DarkSeaGreen","RoyalBlue","DarkGreen","MediumOrchid","BlueViolet"];
 	this.color_map = {};
 	this.emojis = [
 		{name:"thumb_up",code:"em-thumbsup"},
@@ -34,7 +34,8 @@ var Chatter = function(){
 		{name:"item_date",code:"em-date"},
 		{name:"item_chicken",code:"em-chicken"},
 	];
-	this.emoji_css = "<style>i.em{font-size:14px;}div#previewbox{background-color:white;border-radius:5px;border:solid 1px #c0c0c0;min-height:4em;max-height:8em;overflow-y:auto;width:80%;padding:2px 6px;white-space:pre-wrap;line-height:1.25;}div#emoji_palette{border:solid 1px black;background-color:#ECECEC;display:none;}div#emoji_palette.open{display:block;}div#emoji_palette button{margin:5px;}</style>";
+	this.emoji_css = "<style type='text/css'>i.em{font-size:14px;}div#previewbox{background-color:white;border-radius:5px;border:solid 1px #c0c0c0;min-height:4em;max-height:8em;overflow-y:auto;width:80%;padding:2px 6px;white-space:pre-wrap;line-height:1.25;}div#emoji_palette{border:solid 1px black;background-color:#ECECEC;display:none;}div#emoji_palette.open{display:block;}div#emoji_palette button{margin:5px;}</style>";
+	this.document_root;
 	this.iframe;
 	this.userlist;
 	this.msglist;
@@ -64,11 +65,14 @@ var Chatter = function(){
 		if(!_this.enabled) return;
 		
 		_this.iframe = $("iframe").contents();
+		$("iframe").prop('style','height:720px');
+		_this.document_root = _this.iframe.find("html");
 		_this.userlist	= _this.iframe.find("#userlst_id");
 		_this.msglist	= _this.iframe.find("#msglst_id");
 		_this.msgbox	= _this.iframe.find("#d2l_form textarea");
 		_this.btnsend	= _this.iframe.find("#d2l_form a[id*=z_]");
 		if(_this.userlist.length == 0 && _this.msglist.length == 0){
+			_this.document_root = $("html");
 			_this.userlist	= $("#userlst_id");
 			_this.msglist	= $("#msglst_id");
 			_this.msgbox	= $("#d2l_form textarea");
@@ -76,15 +80,14 @@ var Chatter = function(){
 			if(_this.userlist.length == 0 && _this.msglist.length == 0)
 				return;
 		}
-		$("head").append("<link href='https://afeld.github.io/emoji-css/emoji.css' rel='stylesheet'>");
-		$("head").append(_this.emoji_css);
+		_this.document_root.find("head").append("<link href='https://afeld.github.io/emoji-css/emoji.css' rel='stylesheet'>");
+		_this.document_root.find("head").append(_this.emoji_css);
 		var snd_wrap = $("<div id='snd_wrap' style='display:inline-block;'></div>");
 		_this.btnsend.wrap(snd_wrap);
-		snd_wrap = $("#snd_wrap");
+		snd_wrap = _this.document_root.find("#snd_wrap");
 		_this.chkEnterToSend = $("<input type='checkbox' name='enter_to_send' id='enter_to_send' />").insertAfter(_this.btnsend);
 		$("<label for='enter_to_send'>Enter to Send</label>").insertAfter(_this.chkEnterToSend);
 		$("</br>").insertAfter(_this.btnsend);
-		
 		
 		var previewbox = $("<div id='previewbox'><div>");
 		previewbox.insertAfter(snd_wrap);
@@ -152,7 +155,7 @@ var Chatter = function(){
 		});
 
 		_this.msglist.find("div").each(function(i,o){
-			$(o).prop("style","white-space:pre;background-color:#EFEFEF;margin:5px 0;padding:5px;border-radius:10px;");
+			$(o).prop("style","white-space:pre-wrap;background-color:#EFEFEF;margin:5px 0;padding:5px;border-radius:10px;");
 		});
 
 		_this.msglist.find("h3 span").each(function(i,o){
@@ -204,7 +207,7 @@ var Chatter = function(){
 
 	this.pollMessages = function(){
 		_this.msglist.find("div").each(function(i,o){
-			$(o).prop("style","white-space:pre;background-color:#ECECEC;margin:5px 0;padding:5px;border-radius:10px;");
+			$(o).prop("style","white-space:pre-wrap;background-color:#ECECEC;margin:5px 0;padding:5px;border-radius:10px;");
 			var msg = $(o).html();
 			_this.emojis.forEach(function(o,i){
 				msg = msg.replace(new RegExp(":"+o.name+":",'g'),"<i class='em "+o.code+"'></i>");
